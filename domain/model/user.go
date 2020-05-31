ca4f2c9ddedab0f6b35f74165e68e53d222965fc
ohunyk <ohunyk@gmail.com>
2020-05-31 23:04:44 +0900

    refactoring

---
+++ domain/model/user.go
@@ -0,0 +1,20 @@
+package entities
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Email    string             `bson:"email,omitempty"`
+	Password string             `bson:"password,omitempty"`
+	Nickname string             `bson:"nickname,omitempty"`
+}
+
+//type Repository interface {
+//	GetByEmail(ctx context.Context, email string) (User, error)
+//}
+//
+//type Service interface {
+//	GetByEmail(ctx context.Context, email string) (User, error)
+//}
